c5b5f354d08645aea0298b7d7016f992682615c2
YiXiongyu <1651960291@qq.com>
2020-10-26 16:03:42 +0800

    graphql图结构设计完成

--- src/api/user/user.resolver.ts
+++ src/api/user/user.resolver.ts
@@ -1,4 +1,6 @@
 import { Args, Mutation, Query, Resolver, Subscription, Info, Context, CONTEXT, GqlExecutionContext } from '@nestjs/graphql';
+import { LoginArgs } from './user.args';
+import { LoginRes, User } from './user.model';
 import { UserService } from './user.service';
 @Resolver()
 export class UserResolver {
@@ -6,16 +8,17 @@ export class UserResolver {
     private userService: UserService,
   ) { }
 
-  @Query(
-    returns => String,
-    {
-      name: 'hello',
-      description: '测试',
-      nullable: false,
-    },
-  )
-  async hello(): Promise<string> {
-    return 'world';
+  @Query(returns => User)
+  async test(): Promise<User> {
+    return { userId: 1 };
+  }
+
+  @Mutation(returns => LoginRes, { description: "登陆" })
+  async login(
+    @Args() args: LoginArgs
+  ): Promise<LoginRes> {
+    console.log(args)
+    return { res: true }
   }
 
 }
